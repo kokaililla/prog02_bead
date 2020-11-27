92bbe595a578ee3c1d3d24a9b27f96c2ab90185b
kocskam <61797769+kocskam@users.noreply.github.com>
2020-11-27 01:55:10 +0100

    felület - hibás

---
+++ gui_controller.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt5 import QtWidgets, QtCore, QtGui
+from gui import Ui_MainWindow
+
+class Controller:
+
+    def __init__(self):
+        self.mw = QtWidgets.QMainWindow()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self.mw)
+
+        self.ui.b_Start.clicked.connect(self.clickStart)
+        self.ui.b_Exit.clicked.connect(self.clickExit)
+
+        self.mw.show()
+
+
+
+    def clickExit(self):
+        print("Kiléptem")
+
+    def clickStart(self):
+        pass
+
+#MAIN
+app = QtWidgets.QApplication(sys.argv)
+cntrl = Controller()
+sys.exit(app.exec())
