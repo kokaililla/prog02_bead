74ca9a14c8c4cf1ec548416dcddbc0027301c3a5
kocskam <61797769+kocskam@users.noreply.github.com>
2020-11-29 02:30:09 +0100

    Fájlkezelés

--- gui_controller.py
+++ gui_controller.py
@@ -1,9 +1,11 @@
 import sys
+
 from labirintus import getMaze
 from PyQt5 import QtWidgets, QtCore, QtGui
 from gui import Ui_MainWindow
 from labWin import Ui_Dialog
 from PySide2.QtWidgets import QMainWindow, QDialogButtonBox, QVBoxLayout, QDialog, QMessageBox
+from Player import Player, MissingDataException
 
 class Controller:
 
@@ -14,8 +16,9 @@ class Controller:
 
         self.ui.b_Start.clicked.connect(self.clickStart)
         self.ui.b_Exit.clicked.connect(self.clickExit)
-
         self.ui.b_Result.clicked.connect(self.clickResult)
+
+        self.list = []
 
         self.mw.show()
 
@@ -38,12 +41,59 @@ class Controller:
             QtWidgets.QApplication.instance().quit()
 
     def clickStart(self):
-        pass
+
+        name = "PLayer"
+        try:
+            if name == "":
+                raise Exception('név')
+        except Exception as e:
+            msg = QtWidgets.QMessageBox()
+            msg.setWindowTitle("Hiba")
+            msg.setText("Hiányzó " + str(e) + ".")
+            msg.exec()
+
+        self.curr_time = QtCore.QTime(00, 00, 00)
+        self.timer0 = QtCore.QTimer()
+        self.time = QtCore.QTime(00, 00, 00)
+        self.timer0.setInterval(1000)
+        self.timer0.timeout.connect(self.calc)
+        self.timer0.start()
+        self.ui.lcdNumber.display(str(self.calc()))
+
+    def save2File(self, name, time):
+        try:
+            if name == "":
+                raise MissingDataException("név")
+            if time == "":
+                raise MissingDataException("időtartam")
+
+            fout = open("database.txt", "w")
+            for w in self.list:
+                w.save2File(fout)
+            fout.close()
+        except Exception as e:
+            msg = QtWidgets.QMessageBox()
+            msg.setText('Hiba történt:\n' + e.__str__())
+            msg.exec()
+
+    def loadData(self):
+        fin = open("database.txr", "r")
+        for r in fin:
+            ls = r.split(";")
+            nw = Player(ls[0], ls[1], ls[2])
+            self.list.append(nw)
+        fin.close()
+
+
+    def calc(self):
+        global time
+        self.time = self.time.addSecs(1)
+        self.ui.lcdNumber.display(self.time.toString("hh:mm:ss"))
+
 
     def clickResult(self):
-        pass
-        # self.child_win = labWin.Ui_Dialog(self)
-        # self.child_win.show()
+        name = QtWidgets.QApplication.ui.in_Name.text()
+        print(name)
 
     #main
     # t_maze = getMaze()
